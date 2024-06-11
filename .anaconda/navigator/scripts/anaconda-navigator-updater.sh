3509836236e6617ab9640951612ca2c1b73da08b
Madhumitha-Venkatrajan <123716246+Madhumitha-Venkatrajan@users.noreply.github.com>
2024-06-11 14:45:44 -0400

    "

---
+++ .anaconda/navigator/scripts/anaconda-navigator-updater.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+. /Users/loges/anaconda3/bin/activate /Users/loges/anaconda3
+navigator-updater --latest-version 2.5.2 --prefix /Users/loges/anaconda3 >/Users/loges/.anaconda/navigator/scripts/anaconda-navigator-updater-out-1.txt 2>/Users/loges/.anaconda/navigator/scripts/anaconda-navigator-updater-err-1.txt
---
+++ .anaconda/navigator/scripts/notebook.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+. /Users/loges/anaconda3/bin/activate /Users/loges/anaconda3
+open /Users/loges/anaconda3/bin/jupyter_mac.command >/Users/loges/.anaconda/navigator/scripts/notebook-out-1.txt 2>/Users/loges/.anaconda/navigator/scripts/notebook-err-1.txt
